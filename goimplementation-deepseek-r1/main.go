c56a5483ba8ce5d0bfb7ae724144fc809721143f
Peter Hindes <19994487+PeterHindes@users.noreply.github.com>
2025-02-12 20:58:50 -0700

    implement the uuids

--- goimplementation-deepseek-r1/main.go
+++ goimplementation-deepseek-r1/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	_ "bytes"
+	"crypto/rand"
 	"database/sql"
 	"fmt"
 	_ "io"
@@ -315,6 +316,12 @@ func deleteOldTasks(c *gin.Context) {
 }
 
 func generateUUID() string {
-	// Implement proper UUID generation
-	return fmt.Sprintf("%d", time.Now().UnixNano())
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		return fmt.Sprintf("%d", time.Now().UnixNano())
+	}
+	b[6] = (b[6] & 0x0f) | 0x40 // Version 4
+	b[8] = (b[8] & 0x3f) | 0x80 // Variant RFC4122
+	return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 }
